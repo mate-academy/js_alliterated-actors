385f13efa313358ed29e52e4249496c23e1e8205
yojikyo <yojikyo@gmail.com>
2019-04-13 06:21:57 +0300

    'solution'

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -23,6 +23,14 @@
  */
 function alliteratedActors(names) {
   // write code here
+  return names.find(
+    fullName => {
+      const firstLetter = fullName[0];
+      return fullName.split(' ').every(
+        name => name[0] === firstLetter
+      );
+    }
+  );
 }
 
 module.exports = alliteratedActors;
