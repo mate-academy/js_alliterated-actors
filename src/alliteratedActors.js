5f1c2d15bbe504ec97db0a2f3ffae9807061f58a
vladyslav223 <vladyslav223@gmail.com>
2019-08-19 21:00:47 +0300

    actors done

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -23,6 +23,15 @@
  */
 function alliteratedActors(names) {
   // write code here
+  const callback = (name) => {
+    let name1 =  name.split(' ')[0];
+    let name2 = name.split(' ')[1];
+    if(name1[0] === name2[0]) {
+      return name;
+      }
+    return undefined;
+  }
+  const resultName = names.find(callback);
 }
 
 module.exports = alliteratedActors;
