a09acd0f129e9b1fcd5700f1b0feb57580ccea49
Rafler <amuronyk@gmail.com>
2019-08-20 15:31:45 +0300

    solution

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -22,7 +22,9 @@
  * @return {string}
  */
 function alliteratedActors(names) {
-  // write code here
+  return names.find((item) => item
+    .split(' ')
+    .every(elem => elem[0] === item[0]));
 }
 
 module.exports = alliteratedActors;
