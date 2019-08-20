bcd7f6405673f5c8c9c4e4dfae9d6941c838a56b
Miroslava <777shadowpriest777@gmail.com>
2019-08-20 08:09:57 +0300

    Solution

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -22,7 +22,14 @@
  * @return {string}
  */
 function alliteratedActors(names) {
-  // write code here
+  return names.find((fullName) => {
+    const firstLetter = fullName[0];
+
+    return fullName.split(' ').every((name) => {
+      return name.charAt(0) === firstLetter;
+    });
+  });
+//     func
 }
 
 module.exports = alliteratedActors;
