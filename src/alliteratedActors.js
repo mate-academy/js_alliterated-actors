ba316cf041de6bb49997d636ada0b8436d977c99
Volodsher <volodsher@gmail.com>
2019-08-20 15:56:22 +0300

    fantastic

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -2,6 +2,6 @@
 
 function alliteratedActors(names) {
   return names.find(name => name.split(' ')
-    .every(el => el[0] === name[0][0], name[0][0]));
+    .every(el => el[0] === name[0][0]));
 }
 module.exports = alliteratedActors;
