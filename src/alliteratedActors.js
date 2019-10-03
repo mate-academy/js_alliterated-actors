4109036579044440efdb1fab33a5938944c886db
Rudenko Artem <artem.rud@bk.ru>
2019-10-03 16:55:37 +0100

    Solution

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -22,7 +22,9 @@
  * @return {string}
  */
 function alliteratedActors(names) {
-  // write code here
+  return names.find((person) => person.split(' ')
+    .every(findLetters => findLetters.startsWith(person[0]))
+  );
 }
 
 module.exports = alliteratedActors;
