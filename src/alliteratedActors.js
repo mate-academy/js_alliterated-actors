f656c7c2800396ba9bb5e1e0a829437e03862b02
Natalia Lypovyk <cardinnata@gmail.com>
2020-01-29 16:05:06 +0200

    Alliterated Actors

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -22,7 +22,11 @@
  * @return {string}
  */
 function alliteratedActors(names) {
-  // write code here
-}
+  const result = names.find(item => item
+    .split(' ')
+    .every(name => name[0] === item[0]));
+
+  return result;
+};
 
 module.exports = alliteratedActors;
