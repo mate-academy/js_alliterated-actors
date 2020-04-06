ebbf2869795718e5e223d0ae52fdd091012a9cb8
Kostya Peniyavskiy <kornelius9898@gmail.com>
2020-04-06 15:39:36 +0300

    solution

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -22,7 +22,9 @@
  * @return {string}
  */
 function alliteratedActors(names) {
-  // write code here
+  return names.find(
+    name => name.split(' ').every(partOfName => partOfName[0] === name[0])
+  );
 }
 
 module.exports = alliteratedActors;
