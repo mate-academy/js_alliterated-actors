956c9863c9128a9a3d062a83d0b89ffe632ae73f
YuriiMatsiv <matsiv2017@ukr.net>
2020-04-08 07:33:15 +0300

    'solution'

--- src/alliteratedActors.js
+++ src/alliteratedActors.js
@@ -22,7 +22,12 @@
  * @return {string}
  */
 function alliteratedActors(names) {
-  // write code here
+  return names.find(someN => {
+    return someN.split(' ')
+      .every(name => {
+        return name[0] === someN[0];
+      });
+  });
 }
 
 module.exports = alliteratedActors;
